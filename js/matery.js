37dc61e8b0771bda8be697b6ba6dd975bdbf328b
Soke-Mile <mylove_and_cn@163.com>
2022-12-26 21:46:27 +0800

    Site updated: 2022-12-26 21:46:22

--- js/matery.js
+++ js/matery.js
@@ -156,20 +156,12 @@ $(function () {
 		$('.nav-item.nav-show').removeClass('nav-show');
 	})
 	
-    $('.m-nav-item>a').on('click',function(){
-            if ($(this).next('ul').css('display') == "none") {
-                $('.m-nav-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(100);
-                $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
-            }else{
-                $(this).next('ul').slideUp(100);
-                $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
-            }
+    
     });
 
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
-});
+$(".nav-menu>li").hover(function(){ $(this).children('ul').stop(true,true).show(); $(this).addClass('nav-show').siblings('li').removeClass('nav-show'); },function(){ $(this).children('ul').stop(true,true).hide(); $('.nav-item.nav-show').removeClass('nav-show'); }) $('.m-nav-item>a').on('click',function(){ if ($(this).next('ul').css('display') == "none") { $('.m-nav-item').children('ul').slideUp(300); $(this).next('ul').slideDown(300); $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show'); }else{ $(this).next('ul').slideUp(300); $('.m-nav-item.m-nav-show').removeClass('m-nav-show'); } });
 
 //黑夜模式提醒开启功能
 setTimeout(function () {
